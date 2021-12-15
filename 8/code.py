dae1a718e444086b3f30cf1e739c225b73da48f7
David Myers <thedavidjmyers@gmail.com>
2021-12-14 22:38:08 -0800

    sets and dicts are powerful. solved part2.

--- 8/code.py
+++ 8/code.py
@@ -3,17 +3,90 @@
 # Date = December 2020
 import re
 from collections import defaultdict
-with open((__file__.rstrip("code.py")+"sample.txt"), 'r') as input_file:
+with open((__file__.rstrip("code.py")+"input.txt"), 'r') as input_file:
     input = input_file.read().split('\n')
-    input = input[:-1] #(this was sooooo annoying that they didn't format the sample like they did the input)
+    # input = input[:-1] #(this was sooooo annoying that they didn't format the sample like they did the input)
     line = [line.split(' | ') for line in input]
     inputs = [input[0].split(' ') for input in line]
     outputs = [x[1].split(' ') for x in line]
-
+    z  = zip(inputs, outputs)
 # digitPatternLengths = {0:6, 1:2, 2:5, 3:5, 4:4, 5:5, 6:6, 7:3, 8:7, 9:6}
-digitPatternsTrue = {0:'abcefg', 1:'cf', 2:'acdeg', 3:'acdfg', 4:'bcdf', 5:'abdgf', 6:'abdefg', 7:'acf', 8:'abcdefg', 9:'abcdfg'}
-for key in digitPatternsTrue.keys():
-        digitPatternsTrue[key] = {str(letter) for letter in digitPatternsTrue[key]}
+# digitPatternsTrue = {0:'abcefg', 1:'cf', 2:'acdeg', 3:'acdfg', 4:'bcdf', 5:'abdgf', 6:'abdefg', 7:'acf', 8:'abcdefg', 9:'abcdfg'}
+# for key in digitPatternsTrue.keys():
+#         digitPatternsTrue[key] = {str(letter) for letter in digitPatternsTrue[key]}
+
+def out_value (d,output):
+    """
+    (d): a dict with keys for digits 0:9 and values of their scrambled 7-segment codes
+    (output): a list of length 4, each item is a 7-segment code
+
+    (out_value): a four-digit integer constructed from the (4) 7-segment codes
+    """
+    out_value = ''
+    for each in output:
+        odigit = digitSegmentizer(each)
+        # print(odigit)
+        for key_i, value_i in d.items():
+            # print(f'digit to decode: {odigit}\n possible match: {value_i} \n value if matched: {key_i}')
+            if value_i == odigit:
+                # print(f'o_digit:{odigit} \nsegment:{value_i} \nresult:{key_i}')
+                out_value +=str(key_i)
+                # print(out_value)
+    return out_value
+
+
+def digitSegmentizer (input):
+    return {str(letter) for letter in input}
+
+running_sum = 0
+
+for input, output in z:
+    input.sort(key = len)
+    d = defaultdict()
+    for each in input:
+        digit = digitSegmentizer(each)
+        segments = {str(letter) for letter in digit}
+        if len(digit) == 2:
+            d[1] = segments             #1
+        elif len(digit) == 3:
+            d[7] = segments             #7
+        elif len(digit) == 4:
+            d[4] = segments             #4
+        elif len(digit) == 7:
+            d[8] = segments             #8
+        elif len(digit) == 5:
+            if (d[4]-d[1]).issubset(segments):
+                d[5] = segments         #5
+            elif len(digit-d[4]) == 2:
+                # print(f'HELLLLLLO \n{digit} \n{d[4]} \n{digit-d[4]}')
+                d[3]=segments           #3
+            else: d[2]=segments          #2
+        elif len(digit) ==6:
+            if len(digit-d[4])==2:
+                d[9] = segments         #9
+            elif len(digit-d[7])==3:
+                d[0] = segments         #0
+            else: d[6] = segments        #6
+
+        # digits decoded: find the related output, decode, add to sum
+    # print(input)
+    # print(output)
+    # print(d)
+    running_sum+=int(out_value(d,output))
+print(running_sum)
+
+            # running_sum += int(out_value)
+
+# print(running_sum)
+
+uni = 0
+for out in outputs:
+    for digit in out:
+        if len(digit) in [2,3,4,7]:
+            uni +=1
+
+print("Part One : "+ str(uni))
+print("Part Two : "+ str(None))
 
 
 '''
@@ -33,64 +106,3 @@ for key_i, value_i in digitPatternsTrue.items():
 test = inputs[0]
 test.sort(key = len)
 '''
-
-def digitSegmentizer (input):
-    return {str(letter) for letter in input}
-
-
-for input in inputs[:3]:
-    input.sort(key = len)
-    d = defaultdict()
-    for each in input:
-        digit = digitSegmentizer(each)
-        # print(digit)
-        # print('i am the test')
-        # print(test)
-        segments = {str(letter) for letter in digit}
-        if len(digit) == 2:
-            d[1] = segments             #1
-        elif len(digit) == 3:
-            d[7] = segments             #7
-        elif len(digit) == 4:
-            d[4] = segments             #4
-        elif len(digit) == 7:
-            d[8] = segments             #8
-        elif len(digit) == 5:
-            if d[4]-d[1] in segments:
-                d[5] = segments         #5
-            elif len(d[4]-digit) == 3:
-                d[2]=segments           #2
-            else: d[3]=segments          #3
-        elif len(digit) ==6:
-            if len(digit-d[4])==2:
-                d[9] = segments         #9
-            elif len(digit-d[7])==3:
-                d[0] = segments         #0
-            else: d[6] = segments        #6
-
-
-    print(f'heres johhny {d}')
-
-'''
-Now I just need to use d to decode the outputs and add them all up.
-'''
-
-# segment_lookup = defaultdict()
-#
-#
-# print(d)
-#
-#
-#
-
-uni = 0
-for out in outputs:
-    for digit in out:
-        if len(digit) in [2,3,4,7]:
-            uni +=1
-
-print("Part One : "+ str(uni))
-
-
-
-print("Part Two : "+ str(None))
